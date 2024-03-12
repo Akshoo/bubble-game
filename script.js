a26b3b7541b34ded7fdcc698caa0fb6ec500ac49
Akshoo <akshayshukla197@gmail.com>
2024-03-13 00:24:38 +0530

    60 sec se 20 sec kar dia

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const restartbtn = document.querySelector(".restart-btn");
 
 
 let highscore=0;
-let time=60;
+let time=20;
  
 const hitReset = function () {
     hit.textContent = Math.floor(Math.random() * 10);
